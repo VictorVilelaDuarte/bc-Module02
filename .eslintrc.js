ab5417d798da13bbb554d6466e4e8a925f077cdd
Victor Vilela Duarte <victorvduarte@gmail.com>
2019-10-16 02:03:58 -0300

    Criação de usuarios terminada.

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,7 +16,7 @@ module.exports = {
   rules: {
     'prettier/prettier': 'error',
     'class-methods-use-this': 'off',
-    'no-params-reassing': 'off',
+    'no-param-reassign': 'off',
     camelcase: 'off',
     'no-unused-vars': ['error', { argsIgnorePattern: 'next' }],
   },
