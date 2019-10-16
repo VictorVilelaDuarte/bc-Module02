b041fb0f6d90fae3fe8f7b4e95d5047bd5bd9498
Victor Vilela Duarte <victorvduarte@gmail.com>
2019-10-16 01:30:05 -0300

    Conexão com banco de dados Postgres do Docker, configurações de padrões de código, entre outras

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,8 @@
-import { Router } from "express";
+import { Router } from 'express';
 
 const routes = new Router();
 
-routes.get("/", (req, res) => {
-  return res.json({ message: "Hello word" });
+routes.get('/', (req, res) => {
+  return res.json({ message: 'Hello word' });
 });
 export default routes;
