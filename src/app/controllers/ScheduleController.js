cb9eee5a45d5c6582803530d416ede50b7b4855a
Victor Vilela Duarte <victorvduarte@gmail.com>
2019-11-17 05:04:20 -0300

    Ajustes de API para consumo da web

--- src/app/controllers/ScheduleController.js
+++ src/app/controllers/ScheduleController.js
@@ -22,6 +22,13 @@ class ScheduleController {
         canceled_at: null,
         date: { [Op.between]: [startOfDay(parsedDate), endOfDay(parsedDate)] },
       },
+      include: [
+        {
+          model: User,
+          as: 'user',
+          attributes: ['name'],
+        },
+      ],
       order: ['date'],
     });
 
--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -2,6 +2,7 @@ import jwt from 'jsonwebtoken';
 
 import * as Yup from 'yup';
 import User from '../models/User';
+import File from '../models/File';
 
 import authConfig from '../../config/auth';
 
@@ -17,7 +18,12 @@ class SessionController {
     const { email, password } = req.body;
 
     // equivalente: const user = await User.findOne({ where: { email: email } });
-    const user = await User.findOne({ where: { email } });
+    const user = await User.findOne({
+      where: { email },
+      include: [
+        { model: File, as: 'avatar', attributes: ['id', 'path', 'url'] },
+      ],
+    });
 
     if (!user) {
       return res.status(401).json({ error: 'User not found' });
@@ -27,12 +33,13 @@ class SessionController {
       return res.status(401).json({ error: 'Password does not match' });
     }
 
-    const { id, nome } = user;
+    const { id, nome, avatar } = user;
     return res.json({
       user: {
         id,
         nome,
         email,
+        avatar,
       },
       token: jwt.sign({ id }, authConfig.secret, {
         expiresIn: authConfig.expiresIn,
